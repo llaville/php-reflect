9fe36cf6c0c0c7b8ce90b9d8cd1e51c313ea363f
Laurent Laville <laurent.laville@gmail.com>
2013-06-21 21:01:26 +0200

    test suite about PHP 5.5 generators, T_YIELD token

---
+++ tests/_files/generator.php
@@ -0,0 +1,12 @@
+<?php
+function gen_one_to_three() {
+    for ($i = 1; $i <= 3; $i++) {
+        // Note that $i is preserved between yields.
+        yield $i;
+    }
+}
+
+$generator = gen_one_to_three();
+foreach ($generator as $value) {
+    print $value . PHP_EOL;
+}
