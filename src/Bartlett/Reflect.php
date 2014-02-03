2a9eb4b79073943cf281796aba0ab6f2a7c9292d
Laurent Laville <laurent.laville@gmail.com>
2014-02-03 17:34:51 +0100

    base code v2RC2

--- src/Bartlett/Reflect.php
+++ src/Bartlett/Reflect.php
@@ -19,13 +19,9 @@ namespace Bartlett;
 use Bartlett\Reflect\Event\AbstractDispatcher;
 use Bartlett\Reflect\ManagerInterface;
 use Bartlett\Reflect\ProviderManager;
-use Bartlett\Reflect\Token;
-use Bartlett\Reflect\Provider;
-use Bartlett\Reflect\Parser\ParserInterface;
 use Bartlett\Reflect\Parser\DefaultParser;
-use Bartlett\Reflect\Builder;
 use Bartlett\Reflect\Tokenizer\DefaultTokenizer;
-use Bartlett\Reflect\Filter\FilenameFilter;
+use Bartlett\Reflect\Model\PackageModel;
 
 /**
  * Reflect analyse your source code with the tokenizer extension.
@@ -44,43 +40,8 @@ use Bartlett\Reflect\Filter\FilenameFilter;
 class Reflect extends AbstractDispatcher implements ManagerInterface
 {
     protected $pm;
-    protected $parsers;
-    protected $tokenizer;
-    protected $builder;
-
-    /**
-     * Class constructor
-     */
-    public function __construct()
-    {
-        $this->parsers = new \SplDoublyLinkedList;
-    }
-
-    /**
-     * Pushes a parser on to the stack.
-     *
-     * @param ParserInterface $parser Instance of your custom parser
-     *
-     * @return void
-     */
-    public function pushParser(ParserInterface $parser)
-    {
-        $this->parsers->push($parser);
-    }
-
-    /**
-     * Pops a parser from the stack.
-     *
-     * @return ParserInterface
-     * @throws \LogicException if parsers stack is empty
-     */
-    public function popParser()
-    {
-        if ($this->parsers->isEmpty()) {
-            throw new \LogicException('You tried to pop from an empty parser stack.');
-        }
-        return $this->parsers->pop();
-    }
+    protected $ast;
+    protected $files;
 
     /**
      * Returns an instance of the current provider manager.
@@ -113,28 +74,27 @@ class Reflect extends AbstractDispatcher implements ManagerInterface
      * @param array $providers (optional) Data source providers to parse at this runtime.
      *                         All providers defined in Provider Manager by default.
      *
-     * @return array List of items parsed from the data source.
+     * @return self for fluent interface
      */
     public function parse(array $providers = null)
     {
-        $this->builder   = new Builder;
-        $this->tokenizer = new DefaultTokenizer;
+        $tokenizer = new DefaultTokenizer;
+        $parser    = new DefaultParser($tokenizer);
+        $this->ast = new \SplObjectStorage;
 
-        if ($this->parsers->isEmpty()) {
-            $parser = new DefaultParser($this->builder);
-            $this->pushParser($parser);
-        }
+        $this->files = array();
+
+        $all = $this->getProviderManager()->all();
 
         if (empty($providers)) {
-            $providers = array_keys(
-                $this->getProviderManager()->all()
-            );
+            $providers = array_keys($all);
         }
 
-        foreach ($this->getProviderManager()->all() as $alias => $provider) {
+        foreach ($all as $alias => $provider) {
             if (!in_array($alias, $providers)) {
                 continue;
             }
+
             // creates the data model of sources referenced by the $alias name
             foreach ($provider as $uri => $file) {
                 $event = $this->dispatch(
@@ -146,35 +106,30 @@ class Reflect extends AbstractDispatcher implements ManagerInterface
                 );
                 if (isset($event['notModified'])) {
                     // uses cached response
-                    $this->builder->buildFromCache($event['notModified']);
+                    $this->buildFromCache($event['notModified']);
                 } else {
                     // live request
-                    $this->parseFile($file);
+                    $ast = $parser->parseFile($file);
+                    $this->files[] = $file;
+                    $this->ast->addAll($ast);
 
-                    foreach ($this->builder->getPackages() as $package) {
-                        $iterator = new FilenameFilter(
-                            $package->getIterator(),
-                            $file->getPathname()
+                    if ($this->getEventDispatcher()->hasListeners('reflect.success')) {
+                        $this->dispatch(
+                            'reflect.success',
+                            array(
+                                'source'   => $alias,
+                                'filename' => $file->getPathname(),
+                                'ast'      => serialize($ast)
+                            )
                         );
-
-                        if (iterator_count($iterator) > 0) {
-                            // end of parsing the file, and sends results to observers
-                            $this->dispatch(
-                                'reflect.success',
-                                array(
-                                    'source'   => $alias,
-                                    'filename' => $file->getPathname(),
-                                    'package'  => $package->getName(),
-                                    'data'     => iterator_to_array($iterator)
-                                )
-                            );
-                        }
                     }
                 }
             }
             // end of parsing the data source provider
             $this->dispatch('reflect.complete', array('source' => $alias));
         }
+
+        return $this;
     }
 
     /**
@@ -184,253 +139,38 @@ class Reflect extends AbstractDispatcher implements ManagerInterface
      */
     public function getPackages()
     {
-        return $this->builder->getPackages();
+        $packages = array();
+
+        foreach ($this->ast as $object) {
+            if ($object instanceof PackageModel) {
+                $packages[ $object->getName() ] = $object;
+            }
+        }
+
+        return $packages;
     }
 
     /**
-     * Gets informations about all traits.
+     * Gets files list parsed
      *
-     * @return array ClassModel Map objects reflecting each trait.
+     * @return array
      */
-    public function getTraits()
+    public function getFiles()
     {
-        return $this->builder->getTraits();
+        return $this->files;
     }
 
     /**
-     * Gets informations about all interfaces.
+     * Build objects from a previous cached request
      *
-     * @return array ClassModel Map objects reflecting each interface.
-     */
-    public function getInterfaces()
-    {
-        return $this->builder->getInterfaces();
-    }
-
-    /**
-     * Gets informations about all classes.
-     *
-     * @return array ClassModel Map objects reflecting each class.
-     */
-    public function getClasses()
-    {
-        return $this->builder->getClasses();
-    }
-
-    /**
-     * Gets informations about all functions.
-     *
-     * @return array FunctionModel Map objects reflecting each function.
-     */
-    public function getFunctions()
-    {
-        return $this->builder->getFunctions();
-    }
-
-    /**
-     * Gets informations about all constants.
-     *
-     * @return array ConstantModel Map objects reflecting each global constant.
-     */
-    public function getConstants()
-    {
-        return $this->builder->getConstants();
-    }
-
-    /**
-     * Gets informations about all includes.
-     *
-     * @return array IncludeModel Map objects reflecting each include.
-     */
-    public function getIncludes()
-    {
-        return $this->builder->getIncludes();
-    }
-
-    /**
-     * Parse the contents of a single file
-     *
-     * @param Symfony\Component\Finder\SplFileInfo $file Current file be parsed
+     * @param array $cacheData List of models store in cache for the current
+     *                         file parsed
      *
      * @return void
      */
-    private function parseFile($file)
+    private function buildFromCache($cacheData)
     {
-        $this->tokenizer->setSourceFile($file);
-
-        $aliases          = array();
-        $namespace        = false;
-        $namespaceEndLine = false;
-        $class            = false;
-        $classEndLine     = false;
-        $interface        = false;
-        $interfaceEndLine = false;
-        $trait            = false;
-        $traitEndLine     = false;
-        $function         = false;
-        $functionEndLine  = false;
-
-        $tokenStack = $this->tokenizer->getTokens();
-
-        while ($tokenStack->valid()) {
-            $tokenStack->next();
-
-            $token = $tokenStack->current();
-
-            if ('T_HALT_COMPILER' == $token[0]) {
-                break;
-            }
-
-            $tokenName  = $token[0];
-            $text       = $token[1];
-            $line       = $token[2];
-
-            if ($tokenName == 'T_STRING') {
-                // make tokens forward compatible
-                $id = $tokenStack->key();
-
-                // since PHP 5.3
-                if (strcasecmp($text, '__dir__') == 0) {
-                    $tokenName = 'T_DIR';
-                } elseif (strcasecmp($text, '__namespace__') == 0) {
-                    $tokenName = 'T_NS_C';
-                } elseif (strcasecmp($text, 'namespace') == 0
-                    && $namespace === false
-                    && $tokenStack[$id - 1][0] != 'T_OBJECT_OPERATOR'
-                ) {
-                    $tokenName = 'T_NAMESPACE';
-                } elseif (strcasecmp($text, 'goto') == 0) {
-                    $tokenName = 'T_GOTO';
-
-                // since PHP 5.4
-                } elseif (strcasecmp($text, '__trait__') == 0) {
-                    $tokenName = 'T_TRAIT_C';
-                } elseif (strcasecmp($text, 'trait') == 0
-                    && $trait === false
-                ) {
-                    $tokenName = 'T_TRAIT';
-                } elseif (strcasecmp($text, 'insteadof') == 0) {
-                    $tokenName = 'T_INSTEADOF';
-                } elseif (strcasecmp($text, 'callable') == 0) {
-                    $tokenName = 'T_CALLABLE';
-
-                // since PHP 5.5
-                } elseif (strcasecmp($text, 'finally') == 0) {
-                    $tokenName = 'T_FINALLY';
-                } elseif (strcasecmp($text, 'yield') == 0) {
-                    $tokenName = 'T_YIELD';
-                }
-                $token[0] = $tokenName;
-                $tokenStack[$id] = $token;
-            }
-
-            $context = array(
-                'aliases'   => $aliases,
-                'namespace' => $namespace,
-                'class'     => $class,
-                'interface' => $interface,
-                'trait'     => $trait,
-                'function'  => $function,
-            );
-
-            if ('T_CLOSE_CURLY' === $tokenName) {
-                if ($namespaceEndLine !== false
-                    && $namespaceEndLine == $line
-                ) {
-                    $namespace        = false;
-                    $namespaceEndLine = false;
-                }
-                if ($classEndLine !== false
-                    && $classEndLine == $line
-                ) {
-                    $class        = false;
-                    $classEndLine = false;
-                }
-                if ($interfaceEndLine !== false
-                    && $interfaceEndLine == $line
-                ) {
-                    $interface        = false;
-                    $interfaceEndLine = false;
-                }
-                if ($traitEndLine !== false
-                    && $traitEndLine == $line
-                ) {
-                    $trait        = false;
-                    $traitEndLine = false;
-                }
-                if ($functionEndLine !== false
-                    && $functionEndLine == $line
-                ) {
-                    $function        = false;
-                    $functionEndLine = false;
-                }
-
-            } else {
-                $token   = false;
-                $request = array(
-                    'context'  => $context,
-                    'tokens'   => $tokenStack,
-                    'filename' => $file->getPathname(),
-                );
-
-                foreach ($this->parsers as $parser) {
-                    $resp = $parser->handle($request);
-
-                    if ($resp === false) {
-                        $this->dispatch(
-                            'parser.error',
-                            array('request' => $request, 'parser' => $parser)
-                        );
-                    } else {
-                        $this->dispatch(
-                            'parser.success',
-                            array('request' => $request, 'response' => $resp)
-                        );
-                    }
-
-                    if ($token === false && $resp !== false) {
-                        // backup token object on first handled request
-                        $token = $resp;
-                    }
-                }
-                $this->dispatch(
-                    'parser.complete',
-                    array('request' => $request, 'response' => $token)
-                );
-
-                if ($token !== false) {
-                    if ($tokenName == 'T_NAMESPACE') {
-                        $namespace        = $token->getName();
-                        $namespaceEndLine = $token->getEndLine();
-
-                    } elseif ($tokenName == 'T_USE') {
-                        if ($class !== false) {
-                            // warning: don't set $trait value
-                            $traitEndLine = $token->getEndLine();
-                        } else {
-                            // namespaces imported
-                            $aliases[ $token->getAlias() ] = $token->getName(false);
-                        }
-
-                    } elseif ($tokenName == 'T_TRAIT') {
-                        $trait        = $token->getName();
-                        $traitEndLine = $token->getEndLine();
-
-                    } elseif ($tokenName == 'T_INTERFACE') {
-                        $interface        = $token->getName();
-                        $interfaceEndLine = $token->getEndLine();
-
-                    } elseif ($tokenName == 'T_CLASS') {
-                        $class        = $token->getName();
-                        $classEndLine = $token->getEndLine();
-
-                    } elseif ($tokenName == 'T_FUNCTION') {
-                        // function or method
-                        $function        = $token->getName();
-                        $functionEndLine = $token->getEndLine();
-                    }
-                }
-            }
-        }
+        // TODO
+        return ;
     }
 }
