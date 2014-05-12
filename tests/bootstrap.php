ef3cdf25e738ee03f5d957451d1aa166571a83a8
Laurent Laville <laurent.laville@gmail.com>
2014-05-12 19:41:03 +0200

    always use composer autoloader, and never more Symfony class loader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,18 +3,4 @@
 $baseDir   = dirname(__DIR__);
 $vendorDir = $baseDir . '/vendor';
 
-require_once $vendorDir . '/nikic/php-parser/lib/bootstrap.php';
-require_once $vendorDir . '/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Symfony\\Component\\Finder'          => $vendorDir . '/symfony/finder',
-    'Symfony\\Component\\EventDispatcher' => $vendorDir . '/symfony/event-dispatcher',
-    'Symfony\\Component\\Console'         => $vendorDir . '/symfony/console',
-    'Symfony\\Component\\ClassLoader'     => $vendorDir . '/symfony/class-loader',
-    'Bartlett\\Tests\\Reflect'            => $baseDir . '/tests',
-    'Bartlett'                            => $baseDir . '/src',
-));
-$loader->register();
+require_once $vendorDir . '/autoload.php';
