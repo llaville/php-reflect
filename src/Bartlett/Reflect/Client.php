abeaf88994cf7ab585bafcf0ca70b064df74c22c
Laurent Laville <laurent.laville@gmail.com>
2015-01-20 14:43:59 +0100

    more phpdoc on api client

--- src/Bartlett/Reflect/Client.php
+++ src/Bartlett/Reflect/Client.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Client for interacting with the API
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_Reflect
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  GIT: $Id$
+ * @link     http://php5.laurent-laville.org/reflect/
+ */
 
 namespace Bartlett\Reflect;
 
@@ -8,6 +20,13 @@ use Bartlett\Reflect\Client\LocalClient;
 /**
  * Client for interacting with the API
  *
+ * @category PHP
+ * @package  PHP_Reflect
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: @package_version@
+ * @link     http://php5.laurent-laville.org/reflect/
+ * @since    Class available since Release 3.0.0-alpha1
  */
 class Client
 {
