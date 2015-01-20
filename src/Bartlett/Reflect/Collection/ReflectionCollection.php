5d257e58fa4de8613311074e2038b13702443432
Laurent Laville <laurent.laville@gmail.com>
2015-01-20 12:22:27 +0100

    more phpdoc blocks on reflection collection

--- src/Bartlett/Reflect/Collection/ReflectionCollection.php
+++ src/Bartlett/Reflect/Collection/ReflectionCollection.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Reflection Collection
+ *
+ * @category PHP
+ * @package  PHP_Reflect
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  GIT: $Id$
+ * @link     http://php5.laurent-laville.org/reflect/
+ */
 
 namespace Bartlett\Reflect\Collection;
 
@@ -12,10 +22,17 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Reflection collection that collect models for the reflection analyser.
+ *
+ * @category PHP
+ * @package  PHP_Reflect
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version  Release: @package_version@
+ * @link     http://php5.laurent-laville.org/reflect/
+ * @since    Class available since Release 3.0.0-alpha2
  */
 class ReflectionCollection extends ArrayCollection
 {
-
     /**
      * {@inheritDoc}
      */
