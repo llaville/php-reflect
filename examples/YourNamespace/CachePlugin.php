0bf60b44c5c61f9bc32d5def7e60a09167556352
Laurent Laville <laurent.laville@gmail.com>
2015-02-18 18:42:24 +0100

    add new example of API analyser:run that use a custom cache plugin version

---
+++ examples/YourNamespace/CachePlugin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace YourNamespace;
+
+use Bartlett\Reflect\Plugin\CachePlugin as BaseClasePlugin;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class CachePlugin extends BaseClasePlugin
+{
+    // all additional code you need
+
+    public function __construct($cache)
+    {
+        if (is_array($cache)) {
+            if (isset($cache['backend']['class'])) {
+                if (stripos($cache['backend']['class'], 'SQLite3Cache')) {
+                    if (isset($cache['backend']['args'])) {
+                        $args = parent::replaceTokens($cache['backend']['args']);
+                    } else {
+                        $args = array();
+                    }
+                    $cache['backend']['args'][0] = new \SQLite3($args[0]);
+                }
+            }
+        }
+
+        parent::__construct($cache);
+    }
+
+    public function activate(EventDispatcherInterface $eventDispatcher)
+    {
+        echo __CLASS__ . ' version is in use', PHP_EOL;
+    }
+}
