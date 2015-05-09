71a0209cc19935b0f3be8c9ecd65563c62158d12
Laurent Laville <laurent.laville@gmail.com>
2015-05-09 09:36:39 +0200

    uses new PHPUnit ResultPrinter class with Monolog (PSR-3 compatible logger)

---
+++ tests/MonologConsoleLogger.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Prints the result of a TestRunner run using Monolog.
+ *
+ * PHP version 5
+ *
+ * @category   PHP
+ * @package    PHP_Reflect
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    GIT: $Id$
+ * @link       http://php5.laurent-laville.org/reflect/
+ * @since      Class available since Release 3.1.0
+ */
+
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FilterHandler;
+use Monolog\Formatter\LineFormatter;
+
+/**
+ * Prints the result of a TestRunner run using Monolog and few handlers.
+ *
+ * - We log all PHPUnit events to a local file "phpunit-phpcompatinfo.log"
+ *   and keep history 30 days
+ * - We log some PHPUnit events, depending of --verbose, --debug and --colors switches,
+ *   directly to the CLI console
+ *
+ * @category   PHP
+ * @package    PHP_Reflect
+ * @subpackage Tests
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://php5.laurent-laville.org/reflect/
+ */
+class MonologConsoleLogger extends Logger
+{
+    /**
+     * Console logger class constructor
+     *
+     * @param string $name  The logging channel
+     * @param string $level The minimum logging level
+     */
+    public function __construct($name = 'YourLogger', $level = Logger::DEBUG)
+    {
+        $stream = new RotatingFileHandler(__DIR__ . '/phpunit-phpreflect-php' . PHP_VERSION_ID . '.log', 30);
+        $stream->setFilenameFormat('{filename}-{date}', 'Ymd');
+
+        $console = new StreamHandler('php://stdout');
+        $console->setFormatter(new LineFormatter("%message%\n", null, true));
+
+        $filter = new FilterHandler($console);
+
+        $handlers = array($filter, $stream);
+
+        parent::__construct($name, $handlers);
+    }
+
+    /**
+     * Returns list of accepted log levels
+     *
+     * @return array
+     */
+    public function getAcceptedLevels()
+    {
+        $handlers = $this->getHandlers();
+        foreach ($handlers as &$handler) {
+            if ($handler instanceof FilterHandler) {
+                return $handler->getAcceptedLevels();
+            }
+        }
+        return array();
+    }
+
+    /**
+     * Defines log levels that will be accepted.
+     *
+     * @param int|array $minLevelOrList A list of levels to accept or a minimum level if maxLevel is provided
+     * @param int       $maxLevel       Maximum level to accept, only used if $minLevelOrList is not an array
+     *
+     * @return void
+     */
+    public function setAcceptedLevels($minLevelOrList = Logger::DEBUG, $maxLevel = Logger::EMERGENCY)
+    {
+        $handlers = $this->getHandlers();
+        foreach ($handlers as &$handler) {
+            if ($handler instanceof FilterHandler) {
+                $handler->setAcceptedLevels($minLevelOrList, $maxLevel);
+                break;
+            }
+        }
+    }
+}
--- tests/bootstrap.dev.php
+++ tests/bootstrap.dev.php
@@ -14,48 +14,8 @@ $loader->addClassMap(
             => __DIR__ . '/Model/GenericModelTest.php',
         'Bartlett\Tests\Reflect\Environment\YourLogger'
             => __DIR__ . '/Environment/YourLogger.php',
-        'Bartlett\LoggerTestListener'
-            =>  __DIR__ . '/../vendor/bartlett/phpunit-loggertestlistener/src/Bartlett/LoggerTestListener.php',
-        'Monolog\Handler\GrowlHandler'
-            =>  __DIR__ . '/../vendor/bartlett/phpunit-loggertestlistener/extra/GrowlHandler.php',
-        'Monolog\Handler\AdvancedFilterHandler'
-            =>  __DIR__ . '/../vendor/bartlett/phpunit-loggertestlistener/extra/AdvancedFilterHandler.php',
     )
 );
 
-use Monolog\Logger;
-use Monolog\Handler\RotatingFileHandler;
-use Monolog\Handler\GrowlHandler;
-use Monolog\Handler\AdvancedFilterHandler;
-
-class Psr3Logger extends Logger
-{
-    public function __construct($name = 'PHPUnit')
-    {
-        // filter to be notified only on end test suite.
-        $filter1 = function($record) {
-            return (preg_match('/^TestSuite(.*)ended\. Tests/', $record['message']) === 1);
-        };
-
-        $stream = new RotatingFileHandler('/var/logs/phpreflect.log', 30);
-        $stream->setFilenameFormat('{filename}-{date}', 'Ymd');
-
-        $handlers = array($stream);
-
-        try {
-            $growl = new GrowlHandler(array(), Logger::NOTICE);
-
-            $filterGrowl = new AdvancedFilterHandler(
-                $growl,
-                array($filter1)
-            );
-            $handlers[] = $filterGrowl;
-
-        } catch (\Exception $e) {
-            // Growl client is probably not started
-            echo $e->getMessage(), PHP_EOL, PHP_EOL;
-        }
-
-        parent::__construct($name, $handlers);
-    }
-}
+require __DIR__ . '/ResultPrinter.php';
+require __DIR__ . '/MonologConsoleLogger.php';
