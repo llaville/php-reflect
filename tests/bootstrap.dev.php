379232933aa1721e3c995281f5b008a933c7ec37
Laurent Laville <laurent.laville@gmail.com>
2015-10-14 20:48:00 +0200

    re-used standard bootstrap file for unit tests

--- tests/bootstrap.dev.php
+++ tests/bootstrap.dev.php
@@ -1,21 +1,6 @@
 <?php
 
-$baseDir   = dirname(__DIR__);
-$vendorDir = $baseDir . '/vendor';
-
-$loader = require_once $vendorDir . '/autoload.php';
-$loader->addClassMap(
-    array(
-        'Bartlett\Tests\Reflect\Analyser\FooAnalyser'
-            => __DIR__ . '/Analyser/FooAnalyser.php',
-        'Bartlett\Tests\Reflect\Analyser\BarAnalyser'
-            => __DIR__ . '/Analyser/BarAnalyser.php',
-        'Bartlett\Tests\Reflect\Model\GenericModelTest'
-            => __DIR__ . '/Model/GenericModelTest.php',
-        'Bartlett\Tests\Reflect\Environment\YourLogger'
-            => __DIR__ . '/Environment/YourLogger.php',
-    )
-);
+include __DIR__ . '/bootstrap.php';
 
 require __DIR__ . '/ResultPrinter.php';
 require __DIR__ . '/MonologConsoleLogger.php';
