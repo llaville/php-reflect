65e11cac0e87f7cc4375fdbfa765f95e742cbea5
Laurent Laville <laurent.laville@gmail.com>
2019-01-20 10:43:21 +0100

    fix issue: thanks to phpStan to help to found it

--- src/Bartlett/Reflect/Model/ClassModel.php
+++ src/Bartlett/Reflect/Model/ClassModel.php
@@ -512,7 +512,7 @@ class ClassModel extends AbstractModel
 
         $modifiers = '';
         if ($this->isFinal()) {
-            $modifier .= ' final';
+            $modifiers .= ' final';
         }
         if ($this->isAbstract()) {
             $modifiers .= ' abstract';
